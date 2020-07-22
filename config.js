134e18c9556bd88a6b02d386b1eba12b140f6271
ShahHassan27 <s4h7hassan@gmail.com>
2020-07-22 21:45:23 +0900

    caps port to PORT for heroku production

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://S4H7:jjBDeJxbCt0CXp7F@estuary-itwfp.mongodb.net/CARSUM?retryWrites=true&w=majority',
+    SECRET_KEY: 'carsum is my secret'
+  };
